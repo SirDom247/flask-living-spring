0caef9dc10249724d7bdad02dceb63d258b410b9
Dominic Udousoro <dominicudousoro@gmail.com>
2024-05-06 00:21:02 +0100

    Updating Homepage

--- living_spring/routes.py
+++ living_spring/routes.py
@@ -4,6 +4,6 @@ from flask import render_template
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
