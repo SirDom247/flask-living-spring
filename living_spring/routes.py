9c83e1fdabdb5dac9d43870ec54605ded6b76084
Dominic Udousoro <dominicudousoro@gmail.com>
2024-06-13 08:46:17 +0100

    Updating files

--- living_spring/routes.py
+++ living_spring/routes.py
@@ -1,9 +1,20 @@
 from living_spring import app
 
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        # we can process the form data (e.g., save to database)
+        # but for understanding of what's going on at the backend, let's print it to the console for now
+        print(f'Username: {username}, Email: {email}')
+        return "Form submitted!"
+    return render_template('signup.html')
